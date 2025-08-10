58d04e39b41c875110d6ce1f12cfb0b6bc3501aa
Colton English <colton@sparksis.com>
2025-08-10 00:38:08 -0600

    allow argument to be passed to the build script from dev

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ mkdir -p _site .jekyll-cache  # Required due to some file permission issue
 if [[ "${GITHUB_REPOSITORY}" == "coltonenglish/lanark.house" && "${GITHUB_REF_NAME}" == "main" ]]; then
   bundle exec jekyll build
 else
-  bundle exec jekyll build --drafts
+  bundle exec jekyll build --drafts $@
 fi
 
 ls -la _site
